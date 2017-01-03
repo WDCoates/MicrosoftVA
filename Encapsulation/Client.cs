67823cf7d83ee8441e8ed5b9313e54f73584cd36
David Coates <davidc@ocs-consulting.com>
2017-01-03 11:38:07 +0000

    First Projects

---
+++ Encapsulation/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Client : IClient
+    {
+        public string Name { get; set; }
+
+        private ArrayList applications = new ArrayList();
+
+        public void AddApplication(string application)
+        {
+            applications.Add(application);
+        }
+
+        public void DelApplication(string application)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Encapsulation/IClient.cs
@@ -0,0 +1,11 @@
+﻿namespace Encapsulation
+{
+    public interface IClient
+    {
+        string Name { get; set; }
+
+        void AddApplication(string application);
+
+        void DelApplication(string application);
+    }
+}
