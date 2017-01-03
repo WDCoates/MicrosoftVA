b7b579ce34eff28638c68b8b8ed5a60183c016f5
David Coates <davidc@ocs-consulting.com>
2017-01-03 12:00:22 +0000

    First Abb class

---
+++ Sec4-Abstraction/Ab1class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec4_Abstraction
+{
+    public abstract class Ab1class
+    {
+    }
+}
